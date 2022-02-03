9c0b52b7bc8f26f79e6115dd5da86e2c006441e2
rubenmt <rmatiastiscar@safareyes.es>
2022-02-03 20:39:29 +0100

    ultimo ej

--- src/main/java/utilidades/UtilidadesEmpresa.java
+++ src/main/java/utilidades/UtilidadesEmpresa.java
@@ -6,6 +6,7 @@ import modelos.Empresa;
 import modelos.TipoContrato;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class UtilidadesEmpresa{
 
@@ -40,6 +41,70 @@ public class UtilidadesEmpresa{
         }
         Collections.sort(miLista,Collections.reverseOrder());
         return miLista;
+
+    }
+
+
+
+
+    public static double fondoSalarialEmpresa(Empresa empresa){
+
+        double cont = 0;
+
+        for(Empleado e : empresa.getEmpleados()){
+            cont = cont + e.getContrato().getSalarioBase();
+        }
+        return cont;
+    }
+
+
+
+    public static Empleado getMejorPagado(List<Empresa> empresas){
+
+        List<Empleado> miLista = new ArrayList<>();
+
+        for (Empresa e : empresas){
+            for (Empleado i : e.getEmpleados()){
+                miLista.add(i);
+            }
+        }
+
+        Collections.sort(miLista,Collections.reverseOrder());
+
+
+        return miLista.get(0);
+
+    }
+
+
+
+    public static Map<TipoContrato, List<Empleado>> getEmpleadosPorTipoContrato(Empresa empresas){
+
+        List<Empleado> miLista = new ArrayList<>();
+        for (Empleado e : empresas.getEmpleados()){
+            miLista.add(e);
+        }
+
+        Map<TipoContrato, List<Empleado>> miMapa = miLista.stream().collect(Collectors.groupingBy(empleado -> empleado.getContrato().getTipoContrato()));
+
+        return miMapa;
+    }
+
+
+
+    public Map<Empresa, Map<TipoContrato, List<Empleado>>> getEmpleadosPorTipoContrato(List<Empresa> empresas){
+
+        List<Empleado> milista = new ArrayList<>();
+
+        for (Empresa e : empresas){
+            for (Empleado i : e.getEmpleados()){
+                milista.add(i);
+            }
+        }
+
+
+
+        Map<Empresa, Map<TipoContrato, List<Empleado>>> miMapa = empresas.stream().collect(Collectors.groupingBy(e -> ))
 
     }
 
--- src/test/java/main/pruebaV1.java
+++ src/test/java/main/pruebaV1.java
@@ -114,16 +114,30 @@ public class pruebaV1 {
         Contrato miContrato1 = new Contrato(1200,TipoContrato.OBRAYSERVICIO);
         Contrato miContrato2 = new Contrato(1400,TipoContrato.OBRAYSERVICIO);
         Contrato miContrato3 = new Contrato(2000,TipoContrato.TEMPORAL);
-        Contrato miContrato4 = new Contrato(1700,TipoContrato.INDEFINIDO);
+        Contrato miContrato4 = new Contrato(3400,TipoContrato.INDEFINIDO);
 
-        Empleado empleado1 = new Empleado("48120454T","Pedro","Tejero","Calle Cóndor",
+        Contrato miContrato5 = new Contrato(2300,TipoContrato.INDEFINIDO);
+        Contrato miContrato6 = new Contrato(3100,TipoContrato.INDEFINIDO);
+        Contrato miContrato7 = new Contrato(1000,TipoContrato.TEMPORAL);
+        Contrato miContrato8 = new Contrato(1300,TipoContrato.OBRAYSERVICIO);
+
+        Empleado empleado1 = new Empleado("48120454T","Pedro","Fernández","Calle Cóndor",
                 "657984501",null,miContrato1);
-        Empleado empleado2 = new Empleado("48120499Q","Antonio","López","Calle Abeja",
-                "666418900",null,miContrato2);
-        Empleado empleado3 = new Empleado("48120499Q","Antonio","López","Calle Abeja",
-                "666418900",null,miContrato3);
-        Empleado empleado4 = new Empleado("48120499Q","Antonio","López","Calle Abeja",
-                "666418900",null,miContrato4);
+        Empleado empleado2 = new Empleado("48120499Q","Pepe","López","Calle Abeja",
+                "678415609",null,miContrato2);
+        Empleado empleado3 = new Empleado("48120499Q","Rocio","Rodiguez","Calle Alcalá",
+                "658019934",null,miContrato3);
+        Empleado empleado4 = new Empleado("48120499Q","Triana","López","Calle Águila",
+                "611458912",null,miContrato4);
+
+        Empleado empleado5 = new Empleado("48120454T","Alejandro","Cabrera","Calle Paz",
+                "677889933",null,miContrato5);
+        Empleado empleado6 = new Empleado("48120499Q","Luis","Amieva","Calle Mosto",
+                "661224422",null,miContrato6);
+        Empleado empleado7 = new Empleado("48120499Q","Mercedes","Correa","Calle Santa Marta",
+                "677881100",null,miContrato7);
+        Empleado empleado8 = new Empleado("48120499Q","Jesús","Nieto","Calle Picasso",
+                "6131313",null,miContrato8);
 
         List<Empleado> misEmpleados = new ArrayList<>();
         misEmpleados.add(empleado1);
@@ -131,7 +145,14 @@ public class pruebaV1 {
         misEmpleados.add(empleado3);
         misEmpleados.add(empleado4);
 
+        List<Empleado> misEmpleados2 = new ArrayList<>();
+        misEmpleados.add(empleado5);
+        misEmpleados.add(empleado6);
+        misEmpleados.add(empleado7);
+        misEmpleados.add(empleado8);
+
         Empresa empresa1 = new Empresa("HJP01",misEmpleados,TipoEmpresa.STARTUP);
+        Empresa empresa2 = new Empresa("KSK29",misEmpleados2,TipoEmpresa.NACIONAL);
 
         empleado1.setEmpresa(empresa1);
         empleado2.setEmpresa(empresa1);
@@ -139,11 +160,30 @@ public class pruebaV1 {
         empleado4.setEmpresa(empresa1);
         empresa1.setEmpleados(misEmpleados);
 
+        empleado5.setEmpresa(empresa2);
+        empleado6.setEmpresa(empresa2);
+        empleado7.setEmpresa(empresa2);
+        empleado8.setEmpresa(empresa2);
+        empresa2.setEmpleados(misEmpleados2);
 
 
+        //Prueba ej 3a V2
         //System.out.println(getEmpleadosPorContrato(empresa1,TipoContrato.OBRAYSERVICIO));
 
-        System.out.println(UtilidadesEmpresa.getMileuristasOrdenadosPorSalario(empresa1));
+        //Prueba ej 3b V2
+        //System.out.println(UtilidadesEmpresa.getMileuristasOrdenadosPorSalario(empresa1));
+
+        //Prueba ej 3c V2
+        //System.out.println(UtilidadesEmpresa.fondoSalarialEmpresa(empresa1));
+
+        //Prueba ej 3d V2
+        List<Empresa> misEmpresas = new ArrayList<>();
+        misEmpresas.add(empresa1);
+        misEmpresas.add(empresa2);
+        //System.out.println(UtilidadesEmpresa.getMejorPagado(misEmpresas));
+
+        //Prueba ej 3e V2
+        System.out.println(UtilidadesEmpresa.getEmpleadosPorTipoContrato(empresa1));
     }
     
 }
